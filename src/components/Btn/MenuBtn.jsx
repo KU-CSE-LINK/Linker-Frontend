d5aad40db181a7f21a373548e3ec241955a044f3
sooa@naver.com <leesooa0316@naver.com>
2025-01-28 20:58:55 +0900

    기본 세팅 완료

---
+++ src/components/Btn/MenuBtn.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledBtn = styled.button`
+  display: flex;
+  box-sizing: content-box;
+  width: 280px;
+  height: 52px;
+  padding: 10px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 20px;
+  border: 1px solid #000;
+  // 폰트 내용
+  color: #000;
+  font-family: Pretendard;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  &:hover {
+    background: #3773f5;
+    color: #fff;
+    font-weight: 600;
+    border: none;
+  }
+`;
+
+const MenuBtn = ({ children, onClick, type = "button" }) => {
+  return (
+    <StyledBtn type={type} onClick={onClick}>
+      {children}
+    </StyledBtn>
+  );
+};
+
+export default MenuBtn;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import profileIcon from "../../assets/profileIcon.svg";
+import alarmIcon from "../../assets/alarmIcon.svg";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  max-width: 100%;
+  height: 100px;
+  justify-content: center;
+  align-items: center;
+  > img {
+    position: absolute;
+    right: 20px;
+  }
+`;
+const HeaderTitle = styled.h1`
+  color: #000;
+  font-family: Pretendard;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+  letter-spacing: -1.5px; /* 글자 간격 줄이기 */
+`;
+
+const Header = ({ admin }) => {
+  return (
+    <HeaderContainer>
+      <HeaderTitle>LINKER</HeaderTitle>
+      <img src={admin ? alarmIcon : profileIcon} alt="헤더바 아이콘" />
+    </HeaderContainer>
+  );
+};
+
+export default Header;

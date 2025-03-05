3347247eb4daf78881aa73c56dcead9fcc2ca1ad
KingSeokJin <sjin7629@gmail.com>
2025-03-06 00:32:56 +0900

    [fix] Prettier 적용

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,20 +2,12 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from '../pages/Main';
 import Rental from '../pages/rental/Rental';
 import RentalComplete from '../pages/rental/rentalComplete/RentalComplete';
+
 const Router = () => {
   const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Main />,
-    },
-    {
-      path: '/rental',
-      element: <Rental />,
-    },
-    {
-      path: '/rental/comlete',
-      element: <RentalComplete />,
-    },
+    { path: '/', element: <Main /> },
+    { path: '/rental', element: <Rental /> },
+    { path: '/rental/comlete', element: <RentalComplete /> },
   ]);
 
   return <RouterProvider router={router} />;

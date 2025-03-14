965fbdc2a44459dd06154331539d5b26b0e7f3e2
Hwanggyuun <gka365@naver.com>
2025-03-14 18:10:48 +0900

    router 연결결

--- src/components/button/LendButton.jsx
+++ src/components/button/LendButton.jsx
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const Container = styled.button`
-  color: ${({ disabled }) => (disabled ? '#fff' : '#9D9D9D')};
-  background-color: ${({ disabled }) => (disabled ? '#3773F5' : '#F1F1F1')};
+  color: ${({ disabled }) => (disabled ? '#9D9D9D' : '#fff')};
+  background-color: ${({ disabled }) => (disabled ? '#F1F1F1' : '#3773F5')};
   font-size: 20px;
   font-weight: 700;
   line-height: normal;
@@ -14,14 +14,14 @@ const Container = styled.button`
   gap: 10px;
   border-radius: 10px;
   border: none;
-  cursor: ${({ disabled }) => (disabled ? 'pointer' : 'not-allowed')};
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
 `;
 
-const LendButton = ({ onClick, type = 'button', disabled }) => {
+const LendButton = ({ type = 'button', onClick, disabled }) => {
   const buttonText = disabled ? '대여 불가' : '대여 신청';
 
   return (
-    <Container type={type} disabled={disabled} onClick={disabled ? undefined : onClick}>
+    <Container type={type} disabled={disabled} onClick={onClick}>
       {buttonText}
     </Container>
   );

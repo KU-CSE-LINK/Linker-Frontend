8b25db106718e2f559856ce2126190bffd13791c
sooa@naver.com <leesooa0316@naver.com>
2025-03-22 14:15:03 +0900

    Fix: RentalStatusButton propType 지정

--- src/components/button/RentalStatusButton.jsx
+++ src/components/button/RentalStatusButton.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -22,6 +23,12 @@ const RentalStatusButton = ({ children, onClick, isClicked }) => {
       <ButtonContent $isClicked={isClicked}>{children}</ButtonContent>
     </Container>
   );
+};
+
+RentalStatusButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired,
+  isClicked: PropTypes.bool.isRequired,
 };
 
 export default RentalStatusButton;

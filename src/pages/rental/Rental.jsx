277f92ae9e20239102f9153b746622d8a73e0486
KingSeokJin <sjin7629@gmail.com>
2025-03-25 00:19:15 +0900

    [feat] 대여신청 api 구현

--- src/pages/rental/Rental.jsx
+++ src/pages/rental/Rental.jsx
@@ -1,4 +1,4 @@
-import { createRef } from 'react';
+import { createRef, useState } from 'react';
 import {
   ButtonContainer,
   CheckboxContainer,
@@ -20,6 +20,7 @@ import EquipmentCheckBox from '../../components/equipment/CustomCheckBox.jsx';
 import InputWithLabel from '../../components/input/InputWithLabel.jsx';
 import Header from '../../components/header/Header.jsx';
 import Footer from '../../components/footer/footer.jsx';
+import useRentalRequest from '../../apis/rental/useRentalRequest.js';
 
 const dummyEquipments = [
   { id: 1, name: '블루투스 키보드 & 마우스 세트', totalStock: 10, availableStock: 5 },
@@ -32,6 +33,29 @@ const Rental = () => {
   const nameInputRef = createRef();
   const phoneInputRef = createRef();
   const studentIdInputRef = createRef();
+  const [rentalType, setRentalType] = useState(null);
+  const submitRental = useRentalRequest();
+
+  const handleRentalSubmit = async () => {
+    const selectedIndex = equipmentRefs.findIndex((ref) => ref.current?.checked);
+
+    if (selectedIndex === -1) {
+      alert('하나 이상의 기자재를 선택해주세요.');
+      return;
+    }
+
+    const selectedEquipment = dummyEquipments[selectedIndex];
+
+    const data = {
+      equipmentId: selectedEquipment.id,
+      phoneNumber: phoneInputRef.current?.value || '',
+      name: nameInputRef.current?.value || '',
+      studentId: Number(studentIdInputRef.current?.value) || 0,
+      rentalType: rentalType === 'short' ? '단기' : '장기',
+    };
+
+    submitRental(data);
+  };
 
   return (
     <Container>
@@ -66,21 +90,15 @@ const Rental = () => {
         <ButtonContainer>
           <SubTitleText>대여기간</SubTitleText>
           <SubButtonContainer>
-            <HiddenRadio type="radio" name="rentalPeriod" id="short" />
+            <HiddenRadio type="radio" name="rentalPeriod" id="short" onChange={() => setRentalType('short')} />
             <RadioLabel htmlFor="short">단기</RadioLabel>
 
-            <HiddenRadio type="radio" name="rentalPeriod" id="long" />
+            <HiddenRadio type="radio" name="rentalPeriod" id="long" onChange={() => setRentalType('long')} />
             <RadioLabel htmlFor="long">장기</RadioLabel>
           </SubButtonContainer>
         </ButtonContainer>
 
-        <SubmitButton
-          onClick={() => {
-            console.log(equipmentRefs.map((ref) => ref.current?.checked));
-          }}
-        >
-          대여 신청하기
-        </SubmitButton>
+        <SubmitButton onClick={handleRentalSubmit}>대여 신청하기</SubmitButton>
       </SubContainer>
       <Footer />
     </Container>

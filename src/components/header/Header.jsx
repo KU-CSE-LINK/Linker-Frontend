f3b288ea3e6c00ca199016035bed636f1d5a0784
sooa@naver.com <leesooa0316@naver.com>
2025-03-24 15:59:45 +0900

    Feat: Header에 연결

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import 'react';
 import styled from 'styled-components';
 import profileIcon from '../../assets/profileIcon.svg';
+import postDiscordLink from '../../apis/auth/postDiscordLink';
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -16,6 +17,7 @@ const HeaderTitle = styled.h1`
   font-weight: 900;
   line-height: normal;
   letter-spacing: -1.5px;
+  cursor: pointer;
 `;
 
 const HiddenIcon = styled.img`
@@ -26,7 +28,7 @@ const Header = () => {
   return (
     <HeaderContainer>
       <HiddenIcon src={profileIcon} alt="헤더바 아이콘" />
-      <HeaderTitle>LINKER</HeaderTitle>
+      <HeaderTitle onClick={() => postDiscordLink()}>LINKER</HeaderTitle>
       <img src={profileIcon} alt="헤더바 아이콘" />
     </HeaderContainer>
   );

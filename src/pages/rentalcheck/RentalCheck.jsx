0f098f109ca86e7db11a30db0491526564631c50
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-03-27 18:16:50 +0900

    Refactor use rental hook at rental check

--- src/pages/rentalcheck/RentalCheck.jsx
+++ src/pages/rentalcheck/RentalCheck.jsx
@@ -4,48 +4,43 @@ import { BoxContainer, Container, GuestName, SubText } from './RentalCheck.style
 import Header from '../../components/header/Header';
 import RentalBox from '../../components/rentalbox/RentalBox';
 import Footer from '../../components/footer/footer';
-import getRentalData from '../../hooks/rental/getRentalData';
+import useRental from '../../hooks/rental/useRental.jsx';
 
 const RentalCheck = () => {
   const [searchParams] = useSearchParams();
   const name = searchParams.get('name');
   const studentId = searchParams.get('studentId');
 
-  const [rentalData, setRentalData] = useState([]);
-  const [userName, setUserName] = useState('');
+  const [rentals, setRentals] = useState([]);
+
+  const { getRentals } = useRental();
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getRentalData({ name, studentId });
-        setRentalData(data);
-        if (data.length > 0) setUserName(data[0].name);
-        console.log(data);
-      } catch (error) {
-        alert('대여 내역 조회 중 문제가 발생했습니다.');
-        console.error('[ERROR] 대여 내역 불러오기 실패:', error);
-      }
-    };
-
-    if (name && studentId) {
-      fetchData();
+    if (!name || !studentId) {
+      return;
     }
-  }, [name, studentId]);
+
+    (async () => {
+      getRentals({ name, studentId }).then((res) => {
+        setRentals(res);
+      });
+    })();
+  }, []);
 
   return (
     <Container>
       <Header />
       <SubText>
-        <GuestName>{userName}</GuestName>님의 신청내역
+        <GuestName>{name}</GuestName>님의 신청내역
       </SubText>
       <BoxContainer>
-        {rentalData.map((rental) => (
+        {rentals.map((rental) => (
           <RentalBox
             key={rental.id}
-            itemName={rental.itemName}
-            imageSrc={rental.imageSrc}
+            itemName={''}
+            imageUrl={rental.imageUrl}
             status={rental.status}
-            rentalDate={rental.rentalDate}
+            rentalDate={rental.createdAt}
             returnDate={rental.returnDate}
           />
         ))}

1da1a8c91c3d6dfdf7f35a65a1fb620e09a1a7a5
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-03-27 17:42:36 +0900

    Refactor use auth hook at login

--- src/hooks/auth/useAuth.jsx
+++ src/hooks/auth/useAuth.jsx
@@ -1,14 +1,22 @@
 import 'react';
 import useApi from '../useApi.jsx';
+import { useNavigate } from 'react-router-dom';
 
 function useAuth() {
   const { api } = useApi();
+  const navigate = useNavigate();
 
   const sendLoginUrl = () => {
     api.post('/login/url', {});
   };
 
-  return { sendLoginUrl };
+  const login = (code) => {
+    api.post('/login', { code }).then(() => {
+      navigate('/admin');
+    });
+  };
+
+  return { sendLoginUrl, login };
 }
 
 export default useAuth;

e94f2475498a9f1bf59a03bf6285161fc77ee06c
sooa@naver.com <leesooa0316@naver.com>
2025-03-27 17:10:22 +0900

    Fix: useUpdateState 구조 변경

--- src/apis/admin/useUpdateStatus.js
+++ src/apis/admin/useUpdateStatus.js
@@ -1,38 +1,27 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
 
-const useUpdateStatus = (id, status) => {
-  const [newStatus, setNewStatus] = useState('');
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if (!id || !status) return;
-    const updateStatus = async () => {
-      setError(null);
-      axios.post(
+const useUpdateStatus = () => {
+  const updateStatus = (id, status) => {
+    return axios
+      .post(
         'http://localhost:8080/admin/rental',
         {
           id: id,
           status: status,
         },
         {
-          headers: {
-            'Content-Type': 'application/json',
-          },
           withCredentials: true,
-        }
-          .then((response) => {
-            setNewStatus(response.data.status);
-          })
-          .catch((err) => {
-            setError(err);
-          }),
-      );
-    };
+        },
+      )
+      .then((response) => {
+        return response.data.status;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    updateStatus();
-  }, [id, status]);
-  return { newStatus, error };
+  return updateStatus;
 };
 
 export default useUpdateStatus;

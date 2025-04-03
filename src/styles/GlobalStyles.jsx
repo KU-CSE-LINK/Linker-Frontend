134b14e9c8e7c9fb8568e5f9af103cf22a2c1cfd
Hwanggyuun <gka365@naver.com>
2025-04-03 21:04:35 +0900

    미디어 쿼리 기본 설정 추가와 화면 가운데 정렬렬

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -1,15 +1,28 @@
 import { fonts } from './Fonts';
 import { createGlobalStyle } from 'styled-components';
+const breakPoints = [576, 768, 992, 1200];
+export const mediaQueries = breakPoints.map((bp) => `@media (max-width: ${bp}px)`);
 
-const GlobalStyles = createGlobalStyle`
+export const GlobalStyles = createGlobalStyle`
   ${fonts}
 
   * {
     font-family: 'Pretendard', sans-serif;
+    
   }
   p {
     margin: 0;
   }
-`;
+  #root {
+    max-width:800px;
+    margin: 0 auto;
 
-export default GlobalStyles;
+  ${mediaQueries[1]} {
+    max-width: 768px;
+  }
+
+  ${mediaQueries[0]} {
+    max-width: 576px;
+  }
+  } 
+`;

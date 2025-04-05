7d4592e25f7dc2ca963bf4eceb9f8743dc5f6485
sooa@naver.com <leesooa0316@naver.com>
2025-04-06 01:17:00 +0900

    Feat: Header 홈으로 리다이렉트 구현

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const HeaderContainer = styled.div`
@@ -7,20 +8,22 @@ const HeaderContainer = styled.div`
   justify-content: center;
   align-items: center;
 `;
-
 const HeaderTitle = styled.h1`
   color: #000;
   font-size: 40px;
+  font-style: normal;
   font-weight: 900;
+  line-height: normal;
   letter-spacing: -1.5px;
+  cursor: pointer;
 `;
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <HeaderContainer>
-      <HeaderTitle>LINKER</HeaderTitle>
+      <HeaderTitle onClick={() => navigate('/')}>LINKER</HeaderTitle>
     </HeaderContainer>
   );
 };
-
 export default Header;

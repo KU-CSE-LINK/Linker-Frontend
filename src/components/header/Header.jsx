4bea1a5dd14700a6d8325530b0b931a68d284d87
sooa@naver.com <leesooa0316@naver.com>
2025-04-06 01:15:47 +0900

    Fix: 유저 아이콘 및 API 연결 삭제

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,37 +1,24 @@
 import 'react';
 import styled from 'styled-components';
-import profileIcon from '../../assets/profileIcon.svg';
-import useAuth from '../../hooks/auth/useAuth.jsx';
 
 const HeaderContainer = styled.div`
   display: flex;
   width: 100%;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
 `;
 
 const HeaderTitle = styled.h1`
   color: #000;
   font-size: 40px;
-  font-style: normal;
   font-weight: 900;
-  line-height: normal;
   letter-spacing: -1.5px;
-  cursor: pointer;
-`;
-
-const HiddenIcon = styled.img`
-  visibility: hidden;
 `;
 
 const Header = () => {
-  const { sendLoginUrl } = useAuth();
-
   return (
     <HeaderContainer>
-      <HiddenIcon src={profileIcon} alt="헤더바 아이콘" />
-      <HeaderTitle onClick={sendLoginUrl}>LINKER</HeaderTitle>
-      <img src={profileIcon} alt="헤더바 아이콘" />
+      <HeaderTitle>LINKER</HeaderTitle>
     </HeaderContainer>
   );
 };

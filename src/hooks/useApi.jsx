0b226da809349264861d064e07e4cdcbe96116a9
sooa@naver.com <leesooa0316@naver.com>
2025-04-07 09:46:20 +0900

    Chore: 환경 변수 설정

--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 function useApi() {
   const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: import.meta.env.VITE_ENVIRONMENT == development ? 'http://localhost:8080' : 'https://linker-api.fly.dev',
     withCredentials: true,
   });
 
   const adminApi = axios.create({
-    baseURL: 'http://localhost:8080/admin',
+    baseURL: import.meta.env.VITE_ENVIRONMENT == development ? 'http://localhost:8080/admin' : 'https://linker-api.fly.dev/admin',
     withCredentials: true,
   });
 

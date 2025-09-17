f84240f063694e7ed3dee29f382aefbe4aae3494
hwanggyuun <gka365@naver.com>
2025-09-17 15:19:45 +0900

    refactory: util function

--- src/pages/locker/util/getDirection.js
+++ src/pages/locker/util/getDirection.js
@@ -1,23 +1,16 @@
 export function getDirection(direction, rowCount, colCount, lockers) {
-  let rows = [];
-  if (direction === 'row') {
-    for (let row = 0; row < rowCount; row++) {
-      const rowItems = [];
-      for (let col = 0; col < colCount; col++) {
-        const idx = row * colCount + col;
-        if (lockers[idx]) rowItems.push(lockers[idx]);
-      }
-      rows.push(rowItems);
+  const rows = [];
+  for (let row = 0; row < rowCount; row++) {
+    const rowItems = [];
+    for (let col = 0; col < colCount; col++) {
+      const idx = getIndex(direction, row, col, rowCount, colCount);
+      if (lockers[idx]) rowItems.push(lockers[idx]);
     }
-  } else {
-    for (let row = 0; row < rowCount; row++) {
-      const rowItems = [];
-      for (let col = 0; col < colCount; col++) {
-        const idx = col * rowCount + row;
-        if (lockers[idx]) rowItems.push(lockers[idx]);
-      }
-      rows.push(rowItems);
-    }
+    rows.push(rowItems);
   }
+
   return rows;
+}
+function getIndex(direction, row, col, rowCount, colCount) {
+  return direction === 'row' ? row * colCount + col : col * rowCount + row;
 }
